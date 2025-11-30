8684d7d9d2dd09d35a64cac58203bb7bb8a3576f
Jesus Cano <jcanovel@gmail.com>
2025-11-30 14:23:42 +0000

    Task1

--- Task1.py
+++ Task1.py
@@ -11,6 +11,17 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
+distinct_numbers = {*()} # empty set
+
+for text in texts:
+    distinct_numbers |= {text[0], text[1]}
+
+for call in calls:
+    distinct_numbers |= {call[0], call[1]}
+
+print(f"There are {len(distinct_numbers)} different telephone numbers in the records.")
+
+
 
 """
 TASK 1:
